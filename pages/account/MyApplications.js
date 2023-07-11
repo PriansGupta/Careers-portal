37f356ec05b702045b340e138b2f6b6a0bb60ff1
Priyansh <priyanshg615@gmail.com>
2023-07-11 23:08:35 +0530

    Jobs

--- pages/account/MyApplications.js
+++ pages/account/MyApplications.js
@@ -1,15 +1,15 @@
-import Job from "@/Components/Job";
 import Account from ".";
 import { Container } from "@mui/material";
+import { useSession } from "next-auth/react";
 
 export default function MyApplications() {
- 
-
+  const { data: session } = useSession();
   return (
     <>
       <Account>
         <Container className="JobsContainer">
           <h1 className="JobsHeading">My Applications</h1>
+          <h3 className="JobsHeading">{`Welcome back ${session.user.name}`}</h3>
         </Container>
       </Account>
     </>
