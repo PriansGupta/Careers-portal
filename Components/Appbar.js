06a965ccb0baf2fae16a7435303d728587dccecc
Priyansh Gupta <87969057+PriansGupta@users.noreply.github.com>
2023-08-10 11:55:49 +0530

    Update Appbar.js

--- Components/Appbar.js
+++ Components/Appbar.js
@@ -42,7 +42,7 @@ function Navigation() {
     router.push("/login");
   };
   return (
-    <AppBar position="fixed" color="success">
+    <AppBar position="static" color="warning">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
